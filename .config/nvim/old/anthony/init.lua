0e69f8de2d9603db8ba8857bc05a4547dafd268f
Anthony Durrer <anthony@durrer.pro>
2024-03-15 19:07:57 +0100

    Cleaned up old config

--- .config/nvim/old/anthony/init.lua
+++
@@ -1,2 +0,0 @@
-require("anthony.remap")
-require("anthony.set")
--- .config/nvim/old/plugin/lsp.lua
+++
@@ -1,8 +0,0 @@
-local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
-
---lsp.set_preferences({
---	sign_icons = {}
---})
-lsp.setup()
