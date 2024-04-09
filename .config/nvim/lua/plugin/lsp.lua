720af7df2e85d9642b102dc9b52a1fc59c5c190c
Anthony Durrer <anthony@durrer.pro>
2024-04-09 20:48:56 +0200

    Moved back to cmp from coq due to pip not allowing correct installs

--- .config/nvim/lua/plugin/lsp.lua
+++ .config/nvim/lua/plugin/lsp.lua
@@ -1,9 +1,29 @@
-vim.cmd([[let g:coq_settings = { 'auto_start': v:true , 'keymap.pre_select':v:true}]])
+--vim.cmd([[let g:coq_settings = { 'auto_start': v:true , 'keymap.pre_select':v:true}]])
+
 local lsp_zero = require('lsp-zero')
-local coq = require "coq"
+--local coq = require "coq"
+
 
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require('lspconfig').clangd.setup(coq.lsp_ensure_capabilities({}))
+lsp_zero.extend_cmp()
+
+-- And you can configure cmp even more, if you want to.
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
+
+cmp.setup({
+	formatting = lsp_zero.cmp_format({details = true}),
+	mapping = cmp.mapping.preset.insert({
+		['<CR>'] = cmp.mapping.confirm({select = true}),
+		['<C-space>'] = cmp.mapping.complete(),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+		['<C-f>'] = cmp_action.luasnip_jump_forward(),
+		['<C-b>'] = cmp_action.luasnip_jump_backward(),
+	})
+})
+
+require('lspconfig').clangd.setup({})
