fb8218a898d043ed9111fa63cb82ca3f50371894
Anthony Durrer <anthony@durrer.pro>
2024-04-16 19:27:52 +0200

    Added new way to grep through telescope used for quickfix list sub

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,8 @@ return {
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
     end
 }
